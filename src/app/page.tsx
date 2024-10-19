95d040db93eb17898e2fe6b117ec821f7c39c660
VedantK <plcvedant@gmail.com>
2024-10-19 15:55:06 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,163 @@
+import { HackathonCard } from "@/components/hackathon-card";
+import BlurFade from "@/components/magicui/blur-fade";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import { ProjectCard } from "@/components/project-card";
+import { ResumeCard } from "@/components/resume-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { DATA } from "@/data/resume";
+import Link from "next/link";
+import Markdown from "react-markdown";
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default function Page() {
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+      <section id="hero">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+          <div className="gap-2 flex justify-between">
+            <div className="flex-col flex flex-1 space-y-1.5">
+              <BlurFadeText
+                delay={BLUR_FADE_DELAY}
+                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                yOffset={8}
+                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
+              />
+              <h2 className="text-2xl md:text-3xl text-muted-foreground mb-4 pb-2">DevOps & Cloud Engineer</h2>
+              <BlurFadeText
+                className="max-w-[600px] md:text-xl"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+            </div>
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <Avatar className="size-28 border">
+                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
+              </Avatar>
+            </BlurFade>
+          </div>
+        </div>
+      </section>
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold pb-1">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+      <section id="education">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 7}>
+            <h2 className="text-xl font-bold pb-1">Education</h2>
+          </BlurFade>
+          {DATA.education.map((education, id) => (
+            <BlurFade
+              key={education.school}
+              delay={BLUR_FADE_DELAY * 8 + id * 0.05}
+            >
+              <ResumeCard
+                key={education.school}
+                href={education.href}
+                logoUrl={education.logoUrl}
+                altText={education.school}
+                title={education.school}
+                subtitle={education.degree}
+                period={education.end}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+      <section id="skills">
+        <div className="flex min-h-2 flex-col gap-y-3 ">
+          <BlurFade delay={BLUR_FADE_DELAY * 9}>
+            <h2 className="text-xl font-bold pb-1">Skills</h2>
+          </BlurFade>
+          <div className="flex flex-wrap gap-3">
+            {DATA.skills.map((skill, id) => (
+              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                <Badge key={skill}>{skill}</Badge>
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="projects">
+        <div className="space-y-12 w-full py-10">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  My Projects
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl pt-2">
+                  Check out my latest work
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I&apos;ve worked on a variety of projects, from simple
+                  websites to complex web applications deployment. Here are a few of my
+                  favorites.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <div className="container mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              <ProjectCard
+                title="Automated CI/CD Pipeline"
+                description="Implemented a fully automated CI/CD pipeline using Jenkins, Docker, and Kubernetes for a microservices-based application. Reduced deployment time by 70% and improved reliability."
+                tags={["Jenkins", "Docker", "Kubernetes", "CI/CD"]}
+              />
+              <ProjectCard
+                title="Cloud Migration"
+                description="Migrated an on-premise application to AWS, utilizing services such as EC2, RDS, and S3 for improved scalability and cost-efficiency. Achieved 40% cost reduction and 99.99% uptime."
+                tags={["AWS", "EC2", "RDS", "S3", "Migration"]}
+              />
+              <ProjectCard
+                title="Infrastructure as Code (IaC)"
+                description="Developed and maintained Infrastructure as Code using Terraform for multiple cloud environments. Implemented modular design patterns for reusability and consistency across projects."
+                tags={["Terraform", "IaC", "AWS", "Azure", "GCP"]}
+              />
+              <ProjectCard
+                title="Monitoring and Alerting System"
+                description="Set up a comprehensive monitoring and alerting system using Prometheus and Grafana. Created custom dashboards and alerting rules to improve system observability and reduce MTTR by 50%."
+                tags={["Prometheus", "Grafana", "Monitoring", "Alerting"]}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="contact">
+        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-6">
+          <BlurFade delay={BLUR_FADE_DELAY * 16}>
+            <div className="space-y-3">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Contact
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Get in Touch
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Want to chat? Just shoot me a dm{" "}
+                <Link
+                  href={DATA.contact.social.LinkedIn.url}
+                  className="text-blue-500 hover:underline"
+                >
+                  with a direct question on twitter
+                </Link>{" "}
+                and I&apos;ll respond whenever I can. I will ignore all
+                soliciting.
+              </p>
+            </div>
+          </BlurFade>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/data/resume.tsx
@@ -0,0 +1,83 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Vedant T. Kocharekar",
+  initials: "VK",
+  location: "Mumbai, India",
+  locationLink: "https://www.google.com/maps/place/mumbai",
+  description:
+    "Passionate about automating infrastructure, optimizing deployments, and leveraging cloud technologies to build scalable and resilient systems.",
+  summary:
+    "I am a passionate DevOps and Cloud enthusiast dedicated to optimizing software development and delivery processes. With a strong foundation in automation, continuous integration, and cloud infrastructure, I thrive on transforming complex systems into streamlined, efficient workflows. My experience spans various tools and technologies, enabling me to foster collaboration between development and operations teams, enhance scalability, and improve system reliability.",
+  avatarUrl: "/boy.jpg",
+  skills: [
+    "Linux",
+    "Git",
+    "AWS",
+    "Azure",
+    "Jenkins",
+    "Docker",
+    "Kubernetes",
+    "Terraform",
+    "Ansible",
+    "Prometheus",
+    "Grafana",
+    "Python",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+  ],
+  contact: {
+    email: "hello@example.com",
+    tel: "+123456789",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/VedantTK",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/vedantkocharekar",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "https://mail.google.com/mail/",
+        icon: Icons.email,
+
+        navbar: true,
+      },
+    },
+  },
+
+  education: [
+    {
+      school: "St. Francis Institute of Technology",
+      href: "https://www.sfit.ac.in/",
+      degree: "Bachelor's Degree of Electronics and Telecommunication",
+      logoUrl: "/sfit.jpg",
+      end: "2024",
+    },
+    {
+      school: " Vidyavardhini's Bhausaheb Vartak Polytechnic",
+      href: "https://vbvp.edu.in/",
+      degree: "Diploma in Electronics and Telecommunication",
+      logoUrl: "/bvp.png",
+      end: "2021",
+    },
+  ],
+  
+} as const;
